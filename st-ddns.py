2389a980bde7e00bc032cecd918c980b90125eb4
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-06-23 08:45:10 +0200

    Make script PEP8 compliant

--- st-ddns.py
+++ st-ddns.py
@@ -1,78 +1,80 @@
 #!/usr/bin/env python3
 
-import sys
 import requests
 import ssl
-from deviceID import *
-
-key = 'url'
-value = 'hostID'
+from hashlib import sha256
+from deviceID import get_device_id
 
 # ID pinning to host
-hostIDs = [
-{key:'discovery-v4-1.syncthing.net', value:'SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA'},
-{key:'test', value:'bla'},
-]
+pinning = (
+    (
+        'discovery-v4-1.syncthing.net',
+        'SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA',
+    ),
+)
 
-def verifyHost(host):
-	cert = ssl.get_server_certificate((host, 443))
-	v = ssl.PEM_cert_to_DER_cert(cert)
-	digest = sha256(v).digest()
-	hostID=get_device_id(digest)
-	print(hostID)
-	return hostID
 
-# announce
+def verify_host(host):
+    cert = ssl.get_server_certificate((host, 443))
+    v = ssl.PEM_cert_to_DER_cert(cert)
+    digest = sha256(v).digest()
+    hostID = get_device_id(digest)
+    print(hostID)
+    return hostID
+
+
 def announce():
-	"""
-	"""
-	announce_url='https://discovery-v4-1.syncthing.net/v2/?id=SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA'
+    # FIXME: generate this from pinning tuples
+    announce_url = 'https://discovery-v4-1.syncthing.net/v2/?id=SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA'
 
-	announce_request = requests.post(announce_url, json={ 'addresses': ['tcp://:22202']}, verify=False, cert=('./cert.pem', './key.pem') )
+    payload = {'addresses': ['tcp://:12345']}
+    r = requests.post(
+        announce_url,
+        json=payload,
+        verify=False,
+        # TODO: Make this configurable
+        cert=('./cert.pem', './key.pem'),
+    )
 
-	print(announce_request.status_code)
+    print(r.status_code)
 
-	if announce_request.status_code == 204:
-		pass
-	else:
-		print('Announcemnet failed')
-		sys.exit
+    if r.status_code == 204:
+        pass
+    else:
+        print('Announcemnet failed')
+        exit()
 
-	reannounce_time = announce_request.headers['Reannounce-After']
-	print(reannounce_time)
+    reannounce_time = r.headers['Reannounce-After']
+    print(reannounce_time)
 
-# request
+
 def request():
-	"""
-	"""
-	deviceID=''
+    device_id = ''
 
-	with open('./cert.pem') as f:
-		v = ssl.PEM_cert_to_DER_cert(f.read())
-		digest = sha256(v).digest()
-		deviceID=get_device_id(digest)
+    with open('./cert.pem') as f:
+        v = ssl.PEM_cert_to_DER_cert(f.read())
+        digest = sha256(v).digest()
+        device_id = get_device_id(digest)
 
-	request_url='https://announce.syncthing.net/v2/'
+    request_url = 'https://announce.syncthing.net/v2/'
 
-	r = requests.get(request_url + '?device=' + deviceID, verify=False)
-	ip = r.text.split(':')[5].rsplit('/')[2]
-	print(ip)
+    # FIXME: Use urljoin and friends here.
+    r = requests.get(request_url + '?device=' + device_id, verify=False)
+    ip = r.text.split(':')[5].rsplit('/')[2]
+    print(ip)
+
 
 def main():
-	"""
+    # announce()
+    # request()
 
-	"""
-	#announce()
-	#request()
+    fp = verify_host(pinning[0][0])
 
-	for i in range(0,len(hostIDs)):
-		print(hostIDs[i][key])
+    if fp == pinning[0][1]:
+        print('IDs match')
+    else:
+        print('IDs dont match')
 
-	hID = verifyHost(hostIDs[0][key])
-	if hID == hostIDs[0][value]:
-		print('IDs match')
-	else:
-		print('IDs dont match')
 
 if __name__ == '__main__':
-	main()
+    main()
