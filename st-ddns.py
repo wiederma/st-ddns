d0625e452a3cf01535468d6908d9b4c2ae354f62
Stefan Tatschner <rumpelsepp@sevenbyte.org>
2016-06-23 10:31:12 +0200

    Implement announce with verify host

--- st-ddns.py
+++ st-ddns.py
@@ -1,51 +1,79 @@
 #!/usr/bin/env python3
 
-import requests
 import ssl
 from hashlib import sha256
+import requests
 from deviceID import get_device_id
 
-# ID pinning to host
+# fingerprint pinning to host
 pinning = (
     (
         'discovery-v4-1.syncthing.net',
         'SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA',
     ),
+    (
+        'discovery-v4-2.syncthing.net',
+        'DVU36WY-H3LVZHW-E6LLFRE-YAFN5EL-HILWRYP-OC2M47J-Z4PE62Y-ADIBDQC',
+    ),
+    (
+        'discovery-v4-3.syncthing.net',
+        'VK6HNJ3-VVMM66S-HRVWSCR-IXEHL2H-U4AQ4MW-UCPQBWX-J2L2UBK-NVZRDQZ',
+    ),
+    (
+        'discovery-v6-1.syncthing.net',
+        'SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA',
+    ),
+    (
+        'discovery-v6-2.syncthing.net',
+        'DVU36WY-H3LVZHW-E6LLFRE-YAFN5EL-HILWRYP-OC2M47J-Z4PE62Y-ADIBDQC',
+    ),
+    (
+        'discovery-v6-3.syncthing.net',
+        'VK6HNJ3-VVMM66S-HRVWSCR-IXEHL2H-U4AQ4MW-UCPQBWX-J2L2UBK-NVZRDQZ',
+    ),
 )
 
 
-def verify_host(host):
+def verify_host(host, exp_fp):
     cert = ssl.get_server_certificate((host, 443))
     v = ssl.PEM_cert_to_DER_cert(cert)
     digest = sha256(v).digest()
-    hostID = get_device_id(digest)
-    print(hostID)
-    return hostID
+    fp = get_device_id(digest)
+    if fp == exp_fp:
+        return True
+    return False
 
 
 def announce():
-    # FIXME: generate this from pinning tuples
-    announce_url = 'https://discovery-v4-1.syncthing.net/v2/?id=SR7AARM-TCBUZ5O-VFAXY4D-CECGSDE-3Q6IZ4G-XG7AH75-OBIXJQV-QJ6NLQA'
+    for mapping in pinning:
+        disco_url = 'https://' + mapping[0]
+        payload = {'addresses': ['tcp://:12345']}
 
-    payload = {'addresses': ['tcp://:12345']}
-    r = requests.post(
-        announce_url,
-        json=payload,
-        verify=False,
-        # TODO: Make this configurable
-        cert=('./cert.pem', './key.pem'),
-    )
+        try:
+            if verify_host(*mapping) is False:
+                raise RuntimeError
 
-    print(r.status_code)
+            r = requests.post(
+                disco_url,
+                json=payload,
+                verify=False,
+                # TODO: Make this configurable
+                cert=('./cert.pem', './key.pem'),
+            )
+        # verify_host() raises OSError
+        except OSError:
+            # TODO: Log a warning
+            continue
+        except requests.exceptions.ConnectionError:
+            # TODO: Log a warning here
+            continue
 
-    if r.status_code == 204:
-        pass
-    else:
-        print('Announcemnet failed')
-        exit()
+        if r.status_code != 204:
+            # TODO: Log a warning
+            continue
 
-    reannounce_time = r.headers['Reannounce-After']
-    print(reannounce_time)
+        reannounce_time = r.headers['Reannounce-After']
+        print(reannounce_time)
 
 
 def request():
@@ -65,15 +93,8 @@ def request():
 
 
 def main():
-    # announce()
+    announce()
     # request()
-
-    fp = verify_host(pinning[0][0])
-
-    if fp == pinning[0][1]:
-        print('IDs match')
-    else:
-        print('IDs dont match')
 
 
 if __name__ == '__main__':
